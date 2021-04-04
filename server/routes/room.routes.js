81a22b10aba00e0508190d94e3e1b4287743e527
javier <javiermontes1997@hotmail.com>
2021-04-04 20:01:56 +0200

    Creacón y modificación de rutas para la creación de jugadores e invitaciones a partida

--- server/routes/room.routes.js
+++ server/routes/room.routes.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-const { v4: uuidv4 } = require("uuid");
 const rooms = require("../room.json");
 
 router.get("/rooms", (req, res) => {
   res.status(200).json(rooms);
 });
+router.get("/rooms/:invitationId", (req, res) => {
+  const roomFounded = rooms.find(
+    (room) => room.invitationId === req.params.invitationId
+  );
+  if (roomFounded) res.status(200).json(roomFounded);
+  res.status(404).json({ message: "La invitación no pertenece a niguna sala" });
+});
 router.post("/rooms", (req, res) => {
-  const id = req.body.id;
-  // const id = uuidv4();
-  rooms[id] = { name: req.body.name, users: [] };
-  res.status(201).send({});
+  const { id, invitationId } = req.body;
+  const newRoom = { id, invitationId, users: [] };
+  rooms.push(newRoom);
+  res.status(201).json(newRoom);
 });
 
 module.exports = router;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { v4: uuidv4 } = require("uuid");
 const users = require("../user.json");
 
 router.post("/register", (req, res) => {
+  const { socketId, userName } = req.body;
   const newUser = {};
-  newUser.id = uuidv4();
-  newUser.userName = req.body.userName;
+  newUser.socketId = socketId;
+  newUser.userName = userName;
   users.push(newUser);
   res.status(201).json({ message: "Usuario registrado con éxito" });
 });
